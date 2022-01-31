8349182cfcd7d8427ace81c00f06233b44c733f9
ebeltz <ebbeltz@gmail.com>
2022-01-30 23:52:29 -0500

    fixed bug in helper.js file

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,5 @@
 const { format_date } = require('../utils/helpers');
 test('format_date() returns a date string', () => {
-    const date = new Date('2021-01-30 16:12:03');
-    expect(format_date(date)).toBe('1/30/2021');
+    const date = new Date('2022-01-30 16:12:03');
+    expect(format_date(date)).toBe('1/30/2022');
 });
